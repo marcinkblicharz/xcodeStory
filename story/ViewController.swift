162228024adf2e3115af4bdcab9f90a321d06b7f
Marcin Blicharz <marcin.blicharz@gmail.com>
2023-02-07 19:11:37 +0100

    login try

--- story/ViewController.swift
+++ story/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToWelcome", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToWelcome" {
+            let destinationVC = segue.destination as? WelcomeViewController
+            if let login = loginText.text {
+                destinationVC?.login = login
+            }
+            if let password = passwordText.text {
+                destinationVC?.password = password
+            }
+        }
+    }
 }
 
---
+++ story/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  story
+//
+//  Created by Marcin Blicharz on 07/02/2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    var login : String = ""
+    var password : String = ""
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        welcomeLabel.text = "Hi\n\(login)\nwelcome in App\nby \(password)"
+    }
+
+}
