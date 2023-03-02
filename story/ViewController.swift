3e1e3c5cbaea07f8e5d042ccf93d5207104bcb07
Marcin Blicharz <marcin.blicharz@gmail.com>
2023-03-02 18:17:39 +0100

    add login error label is running

--- story/ViewController.swift
+++ story/ViewController.swift
@@ -13,18 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
-    @StateObject var restLogin = RestLogin()
+    var restLogin = RestLogin()
+    var isLogin : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        restLogin.getLogins(urlLink: "http://localhost:8080/rest/getUsers")
+        print("Size of login table is: " + String(restLogin.vm_logins.count))
     }
 
     @IBAction func signInButton(_ sender: UIButton) {
         
+        isLogin = false
+        errorLabel.isHidden = true
         restLogin.getLogins(urlLink: "http://localhost:8080/rest/getUsers")
-        performSegue(withIdentifier: "goToWelcome", sender: self)
-        
+        print("Size of login table is: " + String(restLogin.vm_logins.count))
+        for ApiUser in restLogin.vm_logins {
+            if loginText.text! == ApiUser.name! && passwordText.text! == ApiUser.password! {
+                print("USER: " + ApiUser.name! + ", PASS: " + ApiUser.password!)
+                performSegue(withIdentifier: "goToWelcome", sender: self)
+                isLogin = true
+            }
+        }
+        if !isLogin {
+            errorLabel.textColor = UIColor.red
+            errorLabel.isHidden = false
+            errorLabel.text = "Login or password is incorrect!"
+            print("Login or password is incorrect!")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -37,6 +54,8 @@ class ViewController: UIViewController {
                 destinationVC?.password = password
             }
         }
+//        restLogin.getLogins(urlLink: "http://localhost:8080/rest/getUsers")
+//        print("Size of login table is: " + String(restLogin.vm_logins.count))
     }
 }
 
