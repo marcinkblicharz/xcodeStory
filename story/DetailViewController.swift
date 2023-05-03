a9c39aacb14c4a9d3d9ba2930089c3f2910a6702
Marcin Blicharz <marcin.blicharz@gmail.com>
2023-05-03 18:32:02 +0200

    change setting for layout of detail view

--- story/DetailViewController.swift
+++ story/DetailViewController.swift
@@ -41,11 +41,12 @@ class DetailViewController: UIViewController {
         print("DetailViewController - viewDidLoad, typeOfElement: ", typeOfElement)
         titleLabel.text = typeOfElement.uppercased()
         
-        infoTextView.layer.borderWidth = 0.25
-        nameTextView.layer.borderWidth = 0.25
-        typeTextView.layer.borderWidth = 0.25
-        valueTextView.layer.borderWidth = 0.25
         dateTextView.layer.borderWidth = 0.25
+        dateTextView.vert
+        valueTextView.layer.borderWidth = 0.25
+        typeTextView.layer.borderWidth = 0.25
+        nameTextView.layer.borderWidth = 0.25
+        infoTextView.layer.borderWidth = 0.25
         
         if typeOfElement == "Cost" || typeOfElement == "Income" {
             dateLabel.isHidden = false
@@ -86,6 +87,10 @@ class DetailViewController: UIViewController {
                     self.typeText.text! = String(self.restCost.acv.type)
                     self.nameText.text! = self.restCost.acv.name
                     self.infoText.text! = self.restCost.acv.info
+                    self.dateTextView.text! = self.restCost.acv.date
+                    self.valueTextView.text! = String(self.restCost.acv.value)
+                    self.typeTextView.text! = String(self.restCost.acv.type)
+                    self.nameTextView.text! = self.restCost.acv.name
                     self.infoTextView.text! = self.restCost.acv.info
                 }
             } else if typeOfElement == "Income" {
@@ -96,24 +101,35 @@ class DetailViewController: UIViewController {
                     self.typeText.text! = String(self.restIncome.aiv.type)
                     self.nameText.text! = self.restIncome.aiv.name
                     self.infoText.text! = self.restIncome.aiv.info
+                    self.dateTextView.text! = self.restIncome.aiv.date
+                    self.valueTextView.text! = String(self.restIncome.aiv.value)
+                    self.typeTextView.text! = String(self.restIncome.aiv.type)
+                    self.nameTextView.text! = self.restIncome.aiv.name
                     self.infoTextView.text! = self.restIncome.aiv.info
                 }
             }
         } else if typeOfElement == "CostType" || typeOfElement == "IncomeType" {
-            dateLabel.isHidden = false
+            dateLabel.isHidden = true
             dateLabel.text = "Name"
-            valueLabel.isHidden = false
+            valueLabel.isHidden = true
             valueLabel.text = "Info"
             typeLabel.isHidden = true
             nameLabel.isHidden = true
             infoLabel.isHidden = true
             dateText.isHidden = true
-            dateText.text = "2023-01-01"
+            dateText.text = "Nazwa"
             valueText.isHidden = true
-            valueText.text = "1234.56"
+            valueText.text = "Rodzaj"
             typeText.isHidden = true
             nameText.isHidden = true
             infoText.isHidden = true
+            dateTextView.isHidden = false
+            dateTextView.text = "Name"
+            valueTextView.isHidden = false
+            valueTextView.text = "Info"
+            typeTextView.isHidden = true
+            nameTextView.isHidden = true
+            infoTextView.isHidden = true
         } else {
             dateLabel.isHidden = false
             dateLabel.text = "Date"
@@ -135,6 +151,16 @@ class DetailViewController: UIViewController {
             nameText.text = "nazwa"
             infoText.isHidden = true
             infoText.text = "opis"
+            dateTextView.isHidden = false
+            dateTextView.text = "2023-01-01"
+            valueTextView.isHidden = false
+            valueTextView.text = "1234.56"
+            typeTextView.isHidden = false
+            typeTextView.text = "przy"
+            nameTextView.isHidden = false
+            nameTextView.text = "nazwa"
+            infoTextView.isHidden = false
+            infoTextView.text = "opis"
         }
     }
     
