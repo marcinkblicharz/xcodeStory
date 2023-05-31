07d1bacf8871944e4e19e4bfd1a4c13542a49a75
Marcin Blicharz <marcin.blicharz@gmail.com>
2023-05-31 13:55:42 +0200

    clearing start class

--- story/ViewController.swift
+++ story/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var restLogin = RestLogin()
     var isLogin : Bool = false
     var restCost = RestCosts()
-    var costLink : String! = ""
-    var incomeLink : String! = ""
     var statusLink : String! = ""
     var serverAddress : String! = ""
     @State var def_srv_add : String = UserDefaults.standard.string(forKey: "SERVER_ADDRESS") ?? ""
@@ -47,7 +45,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         isLogin = false
         errorLabel.isHidden = true
-//        costLink = "http://localhost:8080/rest/getCosts?from=" + getLastMonday()
         
         
         if setServerButton.currentTitle == "local" {
@@ -64,8 +61,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
         
-        costLink = "http://" + serverAddress + "/rest/getCosts?from=" + dateFormatterDay.string(from: getLastMonday())
-        incomeLink = "http://" + serverAddress + "/rest/getIncomes?from=" + dateFormatterDay.string(from: getFirstDayOfMonth())
         statusLink = "http://" + serverAddress + "/rest/getStatus"
         print("\nSize of login table is: " + String(restLogin.vm_logins.count))
         
@@ -82,9 +77,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 print("Get data from JSON, user: " + self.loginText.text! + ", password: " + self.passwordText.text! + " with success!")
                 self.performSegue(withIdentifier: "goToWelcome", sender: self)
                 self.isLogin = true
-//                self.linkText.text = self.costLink
-                print("link for current week costs is: '" + self.costLink! + "'")
-                print("link for current day incomes is: '" + self.incomeLink! + "'")
             } else {
                 print("Get data from JSON, user: " + self.loginText.text! + ", password: " + self.passwordText.text! + " with error!")
                 self.errorLabel.textColor = UIColor.red
@@ -108,12 +100,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
             if let password = passwordText.text {
                 destinationVC?.password = password
-            }
-//            if let linkCosts = self.costLink {
-//                destinationVC?.linkCosts = linkCosts
-//            }
-            if let linkIncomes = self.incomeLink {
-                destinationVC?.linkIncomes = linkIncomes
             }
             if let serverAddress = self.serverAddress {
                 destinationVC?.serverAddress = serverAddress
@@ -143,44 +129,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if setServerButton.currentTitle == "custom" {
             customAddress.isHidden = false
         }
-    }
-    
-    func getLastMonday() -> Date {
-        var pastMonday : Int = 0
-        let dateFormatterFull = DateFormatter()
-        dateFormatterFull.dateFormat = "YYYY/MM/dd HH:mm:ss"
-        var calendar = Calendar.current
-        calendar.firstWeekday = 2
-        let currentDate = Date()
-        var dateComponent = DateComponents()
-        let currentWeek = calendar.component(.weekOfYear, from: currentDate)
-        let dayOfWeek = calendar.component(.weekday, from: currentDate)
-        let dayOfMonth = calendar.component(.day, from: currentDate)
-        if dayOfWeek == 2 {
-            print("Today is Monday")
-        } else if dayOfWeek < 2 {
-            pastMonday = (2 - (7 + 1)) * -1
-            print("Monday was " + String(pastMonday) + "days ago")
-        } else {
-            pastMonday = (2 - dayOfWeek) * -1
-            print("Monday was " + String(pastMonday) + "days ago")
-        }
-        //change if week is empty
-        dateComponent.day = pastMonday * -1 //- 7
-        let lastMonday = Calendar.current.date(byAdding: dateComponent, to: currentDate)
-        print("current week is: " + String(currentWeek) + "., day of week is: " + String(dayOfWeek) + "., day of month is: " + String(dayOfMonth) + "., first day of week: " + dateFormatterDay.string(from: lastMonday!))
-        return lastMonday!
-    }
-    
-    func getFirstDayOfMonth() -> Date {
-        var calendar = Calendar.current
-        let currentDate = Date()
-        var dateComponent = DateComponents()
-        let dayOfMonth = calendar.component(.day, from: currentDate)
-        dateComponent.day = (dayOfMonth - 1) * -1 //- 7
-        let firstDay = Calendar.current.date(byAdding: dateComponent, to: currentDate)
-        print("current day of month is: " + String(dayOfMonth) + "., first day of month: " + dateFormatterDay.string(from: firstDay!))
-        return firstDay!
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
